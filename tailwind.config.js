bbf039846e4b16d4d49d569de83d7c0488c85b26
Utsav Tamang <utmg8355@gmail.com>
2024-10-11 14:46:06 +0000

    OkayGood

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './src/index.html',
+    './node_modules/@fortawesome/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
